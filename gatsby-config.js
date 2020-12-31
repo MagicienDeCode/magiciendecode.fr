bbeb866fafe5a39e51fc97e769a5e4b0811bb2e6
xiang_w10_2020 <xiangfr007@gmail.com>
2021-01-01 00:06:37 +0100

    fix css

--- gatsby-config.js
+++ gatsby-config.js
@@ -61,17 +61,5 @@ module.exports = {
         noheader: true,
       },
     },
-    // after all css plugins
-    {
-      resolve: `gatsby-plugin-purgecss`,
-      options: {
-        printRejected: false, // Print removed selectors and processed file names
-        // develop: true, // Enable while using `gatsby develop`
-        // tailwind: true, // Enable tailwindcss support
-        // whitelist: ['whitelist'], // Don't remove this selector
-        // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
-        // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
-      },
-    },
   ],
 }
